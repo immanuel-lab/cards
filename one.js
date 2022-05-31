336637ad1bc89f807ac79e191d058a8b9df3c976
immanuel <immanuelrajmohan3@gmail.com>
2022-06-01 03:40:30 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,44 @@
+// let x
+// x=5
+
+// x=5+""
+// x=x-2
+// console.log(x,typeof x)
+
+// let name1=15
+//  name1=5+""
+//  gum=name1+10
+// console.log(gum,typeof gum)
+
+
+// const x=12;
+// const save=12?'correct':'wrong'
+// console.log(save);
+
+
+// const func=(num1,num2)=>{
+//     const num3=num1+num2;
+//     return num3;
+// }
+
+// console.log(func(1,2));
+
+
+// const array=[1,2,3,4,5]
+
+
+
+// array.filter(n=>n%2===0).forEach(n=>
+//     console.log(n)
+// );
+
+
+// const arr=[1,2,3,4,5]
+
+// const arr1=arr.reduce((a,b)=>a+b);
+// console.log(arr1);
+
+
+// const arr=[1,2,3]
+
+// arr.map(n=>n*2).forEach(n=>{console.log(n)});
